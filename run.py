0e60912c523b482e360c09ee4e672644fe3f9269
Tearnote <marach5@gmail.com>
2023-01-20 11:54:07 +0000

    Introduce Task class
    Represents a single to-do list entry

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
-from colorama import just_fix_windows_console, Fore, Back, Style
+from colorama import just_fix_windows_console, Fore, Style
+
+
+class Task:
+    """A single entry of a to-do list
+
+    :param body: Body text of the task
+    :type body: str
+    :param done: `True` if the task is marked as completed, defaults to `False`
+    :type done: bool, optional
+    """
+
+    def __init__(self, body, done=False):
+        """Constructor method
+        """
+        self.body = body
+        self.done = done
+
 
 just_fix_windows_console()
 
-print(Fore.GREEN + "Hello" + Fore.BLUE + " World" + Style.RESET_ALL + "!")
+task = Task(Fore.GREEN + "Hello" + Fore.BLUE + " World" + Style.RESET_ALL + "!")
+print(task.body)
