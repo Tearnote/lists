c41ffe88038c3cb7cc8610687848c0ccd4ff0266
Tearnote <marach5@gmail.com>
2023-01-23 10:35:42 +0000

    Add class to store and manage configuration

--- run.py
+++ run.py
@@ -1,6 +1,34 @@
 from colorama import just_fix_windows_console, Fore, Style
 
 
+class Config:
+    """Facility for accessing global app configuration
+    """
+
+    _fields = {
+        # How to print tasks that are marked done
+        # "yes": Print the full text
+        # "hidden": Print replacement text
+        # "no": Skip printing the task entirely
+        "print_done_tasks": "hidden"
+    }
+
+    @staticmethod
+    def get(field):
+        """Retrieve a config value
+
+        :param field: Value name
+        :type field: str
+        :return: The value of the config field
+        :rtype: depends on the field
+        """
+        return Config._fields[field]
+
+    @staticmethod
+    def set(field, value):
+        Config._fields[field] = value
+
+
 class Task:
     """A single entry of a to-do list
 
