353d375b7f5dcb0e99eb8773a9f38579641b4d74
Tearnote <marach5@gmail.com>
2023-01-25 10:43:14 +0000

    Move List class into a separate file

--- run.py
+++ run.py
@@ -1,58 +1,11 @@
 from enum import Enum, auto
-from functools import reduce
 
 from colorama import just_fix_windows_console, Fore, Style
 
 from util import put
 from config import Config
 from task import Task
-
-
-class List:
-    """An ordered, printable list of tasks
-
-    Keep in mind that the tasks are printed starting from 1. When accessing
-    tasks based on user input, you will need to subtract one.
-
-    :param name: Name of the list
-    :type name: str
-    :param tasks: List of :class:`Task`s in the list. Can be freely manipulated
-    :type tasks: list, optional
-    """
-
-    def __init__(self, name, tasks=None):
-        """Constructor method
-        """
-        if tasks is None:
-            tasks = []
-        self.name = name
-        self.tasks = tasks
-
-    def __str__(self):
-        """Print the list as numbered tasks, one per line
-
-        Done tasks are greyed out.
-        """
-        result = ""
-        for i in range(len(self.tasks)):
-            task = self.tasks[i]
-            if Config.get("print_done_tasks") == "no" and task.done:
-                continue
-            if not task.done:
-                result += Fore.LIGHTWHITE_EX
-            else:
-                result += Fore.LIGHTBLACK_EX
-            result += "#" + str(i + 1) + " " + str(task) + "\n"
-
-        return result
-
-    def count_done(self):
-        """Count how many tasks are done
-
-        :return: The number of tasks in the list that are done
-        :rtype: int
-        """
-        return reduce(lambda acc, t: acc + 1 if t.done else acc, self.tasks, 0)
+from list import List
 
 
 class UserInput:
