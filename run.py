6cae05cf6f827150eae0ec2e75f64cdcc99b3dc4
Tearnote <marach5@gmail.com>
2023-01-25 10:33:13 +0000

    Move put function into a separate file

--- run.py
+++ run.py
@@ -2,15 +2,7 @@ from enum import Enum, auto
 from functools import reduce
 
 from colorama import just_fix_windows_console, Fore, Style
-
-
-def put(text):
-    """Print a string without any appended newline
-
-    :param text: The string to print
-    :type text: str
-    """
-    print(text, end="")
+from util import put
 
 
 class Config:
---
+++ util.py
@@ -0,0 +1,7 @@
+def put(text):
+    """Print a string without any appended newline
+
+    :param text: The string to print
+    :type text: str
+    """
+    print(text, end="")
