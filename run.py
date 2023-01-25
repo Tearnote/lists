7956d1387935058b18caa52bfdc1bf6ed2947cba
Tearnote <marach5@gmail.com>
2023-01-25 10:33:50 +0000

    Move Config class into a separate file

--- run.py
+++ run.py
@@ -3,34 +3,7 @@ from functools import reduce
 
 from colorama import just_fix_windows_console, Fore, Style
 from util import put
-
-
-class Config:
-    """Facility for accessing global app configuration
-    """
-
-    _fields = {
-        # How to print tasks that are marked done
-        # "yes": Print the full text
-        # "hidden": Print replacement text
-        # "no": Skip printing the task entirely
-        "print_done_tasks": "hidden"
-    }
-
-    @classmethod
-    def get(cls, field):
-        """Retrieve a config value
-
-        :param field: Value name
-        :type field: str
-        :return: The value of the config field
-        :rtype: depends on the field
-        """
-        return cls._fields[field]
-
-    @classmethod
-    def set(cls, field, value):
-        cls._fields[field] = value
+from config import Config
 
 
 class Task:
