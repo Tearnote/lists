c5690233cec974c4b3b6f6562c9313fe343ea7b9
Tearnote <marach5@gmail.com>
2023-01-26 12:50:50 +0000

    Add more properties to config fields

--- config.py
+++ config.py
@@ -2,25 +2,67 @@ class Config:
     """Facility for accessing global app configuration
     """
 
-    _fields = {
-        # How to print tasks that are marked done
-        # "yes": Print the full text
-        # "hidden": Print replacement text
-        # "no": Skip printing the task entirely
-        "print_done_tasks": "hidden"
-    }
+    class Field:
+        """A single config field
 
-    @classmethod
-    def get(cls, field):
-        """Retrieve a config value
-
-        :param field: Value name
-        :type field: str
-        :return: The value of the config field
-        :rtype: depends on the field
+        :param name: Identifier of the field, for use in code
+        :type name: str
+        :param value: The initial value of the field
+        :type value: str
+        :param description: Human-readable description of the field
+        :type description: str
+        :param values: A list of allowed values. If empty, any value is allowed
+        :type values: list
         """
-        return cls._fields[field]
+
+        def __init__(self, name, value, description, values=None):
+            """Constructor method
+            """
+            self.name = name
+            self.value = value
+            self.description = description
+            self.values = [] if values is None else values
+
+    _fields = [
+        Field("print_done_tasks",
+              "hidden",
+              "Show done tasks",
+              ["yes", "no", "hidden"])
+    ]
 
     @classmethod
-    def set(cls, field, value):
-        cls._fields[field] = value
+    def get(cls, name):
+        """Retrieve a config field's value
+
+        :param name: Field name
+        :type name: str
+        :return: The value of the config field
+        :rtype: str
+        """
+        return cls._find_field(name).value
+
+    @classmethod
+    def set(cls, name, value):
+        """Set a config field to a provided value
+
+        :param name: Field name
+        :type name: str
+        :param value: New value to set on the field
+        :type value: str
+        :raises ValueError: The provided value is invalid for the field
+        """
+        field = cls._find_field(name)
+        if len(field.values) > 0 and field.values.count(value) == 0:
+            raise ValueError
+        field.value = value
+
+    @classmethod
+    def _find_field(cls, name):
+        """Retrieve the field object with the given name
+
+        :param name: Field name
+        :type name: str
+        :return: The field object
+        :rtype: :class:`Field`
+        """
+        return next(filter(lambda f: f.name == name, cls._fields))
